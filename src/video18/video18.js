c7c5e7b28fd3abf8568fc7cf7e6040e61cd43f4a
lehuy0210 <nhokhuypro78999@gmail.com>
2025-11-22 17:13:37 +0700

    Buoi 1

---
+++ src/video18/video18.js
@@ -0,0 +1,24 @@
+//number
+
+const myAge = 25;
+const myScore = 9.1;
+
+console.log("video 18")
+console.log(myAge)
+console.log(myScore)
+
+//string
+const name = "hoidanit";
+console.log(name)
+
+//boolean true/false
+const isTuiDepTrai = true;
+console.log(isTuiDepTrai)
+
+//undefined
+let bla;
+console.log(bla);
+
+//null
+let testNUll = null 
+console.log(testNUll);
